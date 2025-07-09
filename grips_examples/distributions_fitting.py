1f9a6fe5ec16641e3af93519220eba1e3336211b
Kerger <kerger.p@gmail.com>
2025-07-09 01:08:45 -0400

    fitting distributions

--- grips_examples/distributions_fitting.py
+++ grips_examples/distributions_fitting.py
@@ -11,6 +11,7 @@ import grips.paper_proxy as ppr
 import grips.normal_proxy as npr
 import os  
 import grips.real_distribution as rd 
+from grips.triangle_proxy import TriangleProxy
 
 from juliacall import Main as jl
 jl.seval('''
@@ -21,12 +22,12 @@ using JuliaQAOA
 ''')
 
 # %%
-num_edges = 7
+num_edges = 10
 edge_probability = 0.5
-num_graphs = 5
+num_graphs = 10
 graphs = [nx.erdos_renyi_graph(num_edges, edge_probability) for _ in range(num_graphs)]
 
-realdist = rd.get_homogeneous_distribution(graphs)/len(graphs)
+realdist = rd.get_homogeneous_distribution(graphs)
 print(realdist)
 realdist.shape
 
@@ -68,13 +69,13 @@ def loss_function(params, realdist, num_constraints, num_qubits):
 #%%
 # Initial guess and bounds
 initial_params = [0.0, 0.0, 1.0, 1.0]  # reasonable starting point
-num_constraints = 7  # Number of constraints
-num_qubits = 7  # Number of qubits
+num_constraints = num_edges  # Number of constraints
+num_qubits = graphs[0].number_of_nodes()  # Number of qubits
 bounds = [
     (0, None),   # h_tweak_sub >= 0
-    (-5, 5),     # hc_tweak_add can be small positive/negative
-    (0.1, 10),   # l_tweak_mul > 0
-    (0.1, 10),   # r_tweak_mul > 0
+    (-10, 10),   # hc_tweak_add can be small positive/negative
+    (0.1, 20),   # l_tweak_mul > 0
+    (0.1, 20),   # r_tweak_mul > 0
 ]
 
 result = minimize(
@@ -83,14 +84,28 @@ result = minimize(
     args=(realdist, num_constraints, num_qubits),
     method='L-BFGS-B',
     bounds=bounds,
-    options={'maxiter': 500}
+    options={'maxiter': 10000}
 )
 
 fitted_params = result.x
 print("Fitted parameters:", fitted_params)
+fitted_proxy = TriangleProxy(
+    num_constraints=num_constraints,
+    num_qubits=num_qubits,
+    h_tweak_sub=fitted_params[0],
+    hc_tweak_add=fitted_params[1],
+    l_tweak_mul=fitted_params[2],
+    r_tweak_mul=fitted_params[3]
+)
 
 
 #%%
-triangle_results = QAOA_proxy(fitted_proxy, gammas, betas)
-print("Triangle Proxy Results:", triangle_results)
+gammas = np.linspace(0, np.pi, 10)  # Gamma values for QAOA
+betas = np.linspace(0, np.pi, 10)  # Beta values for QAOA
+fitted_triangle_results = QAOA_proxy(fitted_proxy, gammas, betas)
+print("Fitted Proxy Results:", fitted_triangle_results)
 
+final_amplitudes = fitted_triangle_results[-1]
+expectation = QAOA_proxy_expectation(fitted_proxy, final_amplitudes)
+print("Expectation value:", expectation)
+# %%
