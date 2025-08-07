e596a6eb36d0a415997818df50bbdf69a726211e
Spencer Lee <leespen1@msu.edu>
2025-08-06 20:35:05 -0400

    Revert "BUG FIX: fixed incorrect homogeneous distribution computation. When taking averages, was dividing by number of cost occurences multiple times, one for each bitstring with that cost. Now, only divides by the number of cost occurences once."
    This reverts commit 6cf21f347220eceac763886797ece264736641f1.

--- grips/real_distribution.py
+++ grips/real_distribution.py
@@ -225,7 +225,8 @@ def get_homogeneous_distribution_from_costs(costs, real_distribution, max_possib
             homogeneous_distribution[bitstring_cost, :, :] += real_distribution[bitstring_i, :, :]
 
     # Take average over number of bitstrings with cost c'
-    for bitstring_cost_int in range(num_costs):
+    for bitstring_cost in costs:
+        bitstring_cost_int = int(bitstring_cost)
         homogeneous_distribution[bitstring_cost_int, :, :] /= num_cost_occurences[bitstring_cost_int]
 
     return homogeneous_distribution
