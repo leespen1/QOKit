aae0660377d946fdf1cad98414635c89ee7d9c13
Spencer Lee <leespen1@msu.edu>
2025-08-27 21:06:27 -0400

    Added sanity check that scipy.minimize is working correctly

---
+++ grips_tests/minimize_sanity_check.py
@@ -0,0 +1,18 @@
+import scipy
+import numpy as np
+
+def g(y):
+    return 2*y[1] + 3*y[2]
+
+
+def make_f(n):
+    def f(x):
+        print("x = ", x)
+        y = x[:n]
+        z = y[n:]
+        return np.linalg.norm(y) + np.linalg.norm(z)
+    return f
+
+
+x0 = np.hstack([np.array([1,2]), np.array([1,2])])
+result = scipy.optimize.minimize(make_f(2), x0, method="COBYLA", args=())
