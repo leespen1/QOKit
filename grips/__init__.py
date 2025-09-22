501db1306083dc11c4838324ad5d6720665ae87a
Spencer Lee <leespen1@msu.edu>
2025-09-22 15:41:26 -0400

    Moved Juliacall import to QAOA_proxy_interface, which will now be its own submodule (to avoid loading juliacall all the time)

--- grips/__init__.py
+++ grips/__init__.py
@@ -7,31 +7,10 @@ of needing to do "from grips.file_name import function_name".
 """
 import os
 
-# Make the functions written in Julia available (call with `jl.function_name`)
-# (by having this in __init__.py, should be able to simply do from grips import jl)
-from juliacall import Main as jl
-grips_dir = os.path.dirname(os.path.abspath(__file__))
-julia_project_dir = os.path.normpath(os.path.join(grips_dir, "../julia"))
-jl.seval(f'''
-using Pkg
-Pkg.activate("{julia_project_dir}")
-try
-    using JuliaQAOA
-catch e
-    if isa(e, ArgumentError)
-        println("Encountered error during 'using JuliaQAOA', instantiating ...")
-        Pkg.instantiate()
-        using JuliaQAOA
-    else
-        rethrow(e)
-    end
-end
-''')
-
 from .normal_proxy import *
 from .paper_proxy import *
 from .plot_utils import *
-from .QAOA_proxy_interface import *
+#from .QAOA_proxy_interface import * # Removing this so that julicall only needs to be loaded when using the proxy interface
 from .QAOA_simulator import *
 from .real_distribution import *
 from .scipy_additional_optimizers import *
