2e497369cf0514e5c3ebdf06ffa41165f2fee3cd
Spencer Lee <leespen1@msu.edu>
2025-10-23 15:39:24 -0400

    FIXED MAJOR BUG IN GPU PROXY DISTRIBUTION GENERATION

--- julia/src/utils.jl
+++ julia/src/utils.jl
@@ -14,7 +14,7 @@ end
 function allocate_homodist(num_constraints::Integer, num_qubits::Integer)
     num_costs = num_constraints + 1
     num_distances = num_qubits + 1
-    distribution = Array{Float64, 3}(undef, num_costs, num_distances, num_costs)
+    distribution = fill(1.0, num_costs, num_distances, num_costs)
     return distribution
 end
 
@@ -59,7 +59,7 @@ function gpu_multi_proxy_mse(
     show_progress=false
 )
     proxy = proxies[1] 
-     
+
     # Could make costs arrays of UInt16's to save space.
     costs_prime = collect(0:proxy.num_constraints) |> CuArray
     distances = reshape(collect(0:proxy.num_qubits), (1, :)) |> CuArray
@@ -67,22 +67,26 @@ function gpu_multi_proxy_mse(
     num_elements_in_homodist = (1+proxy.num_qubits)*(1+proxy.num_constraints)^2
     mse_batches = Vector{Float64}[]
     sampled_homodist_gpu = CuArray(sampled_homodist)
+
     if normalize
         sampled_homodist_volume = sum(sampled_homodist_gpu)
         sampled_homodist_gpu ./= sampled_homodist_volume
     end
+
     @showprogress enabled=show_progress for (i, proxy_batch_gpu) in enumerate(Iterators.partition(proxies, batch_size))
         proxy_batch_gpu_reshaped = reshape(proxy_batch_gpu, (1,1,1,:)) |> CuArray
+
         homodists_gpu = N_cost_distance_distribution.(
             proxy_batch_gpu_reshaped, costs_prime, distances, costs_unprime
         )  
+
         if normalize # Normalize each homodist
-            homodist_gpu_volumes = sum(sampled_homodist_gpu, dims=(1,2,3))
+            homodist_gpu_volumes = sum(homodists_gpu, dims=(1,2,3))
             homodists_gpu ./= homodist_gpu_volumes # Vectorized, should divide each 3D slice
         end
+
         # TODO mse_batch_gpu computation could be reduced to one mapreduce call
         homodists_gpu .-= sampled_homodist_gpu # sampled_homodist is 3D, this will be repeated over 4th dimension
-
         mse_batch_gpu = mapreduce(x -> x*x, +, homodists_gpu, dims=(1,2,3)) # Reduce first 3 dims, leave 4th dim
         mse_batch_gpu ./= num_elements_in_homodist
 
@@ -98,25 +102,33 @@ function cpu_multi_proxy_mse(
     normalize=true,
     show_progress=false,
 )
+    proxy = proxies[1] 
+     
+    # Could make costs arrays of UInt16's to save space.
+    costs_prime = collect(0:proxy.num_constraints)
+    distances = reshape(collect(0:proxy.num_qubits), (1, :))
+    costs_unprime = reshape(costs_prime, (1, 1, :))
+    num_elements_in_homodist = (1+proxy.num_qubits)*(1+proxy.num_constraints)^2
+    mse_batches = Vector{Float64}[]
+
     if normalize
         sampled_homodist_volume = sum(sampled_homodist)
         sampled_homodist ./= sampled_homodist_volume
     end
-     
-    # Could make costs arrays of UInt16's.
-    proxy = proxies[1] 
-    costs_prime = 0:proxy.num_constraints
-    distances = reshape(0:proxy.num_qubits, (1, :))
-    costs_unprime = reshape(costs_prime, (1, 1, :))
-    num_elements_in_homodist = (1+proxy.num_qubits)*(1+proxy.num_constraints)^2
-    mse_batches = Vector{Float64}[]
+
     @showprogress enabled=show_progress for (i, proxy_batch) in enumerate(Iterators.partition(proxies, batch_size))
         proxy_batch_reshaped = reshape(proxy_batch, (1,1,1,:)) 
-        homodists = N_cost_distance_distribution.(proxy_batch_reshaped, costs_prime, distances, costs_unprime)  
+
+        homodists = N_cost_distance_distribution.(
+            proxy_batch_reshaped, costs_prime, distances, costs_unprime
+        )  
+
         if normalize # Normalize each homodist
             homodist_volumes = sum(homodists, dims=(1,2,3))
             homodists ./= homodist_volumes # Vectorized, should divide each 3D slice
         end
+
+        # TODO mse_batch computation could be reduced to one mapreduce call
         homodists .-= sampled_homodist # sampled_homodist is 3D, this will be repeated over 4th dimension
         mse_batch = mapreduce(x -> x*x, +, homodists, dims=(1,2,3)) # Reduce first 3 dims, leave 4th dim
         mse_batch ./= num_elements_in_homodist
